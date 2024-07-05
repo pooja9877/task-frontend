3bf176b7a7b4c20a6e2d885f92bb24bbce5cbbf8
pooja194 <pooja987743@gmail.com>
2024-07-05 14:57:36 +0530

    changes

---
+++ src/details.js
@@ -0,0 +1,228 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Container, Row, Col } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPhone, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
+import { faFacebook,faLinkedin, faTwitter, faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { Table, Input, Button, Spin, message, Popconfirm, Modal } from "antd";
+import { useParams } from "react-router-dom";
+const Details = () => {
+  const [singleDetails, setSingleDetails] = useState({});
+  const [loader, setLoader] = useState(false);
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoader(true);
+        const response = await axios.get(
+          `http://localhost:5000/companies/${id}`
+        );
+        setSingleDetails(response.data);
+      } catch (error) {
+        console.error(error);
+        message.error("Failed to fetch company details");
+      } finally {
+        setLoader(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <Container>
+      
+      <Row
+        className="mt-4"
+        style={{
+          border: "1px solid #dee2e6",
+          padding: 25,
+          borderRadius: "5px",
+        }}
+      >
+        <Col md={7}>
+          {loader ? (
+            <div>Loading...</div>
+          ) : (
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <img
+                src={singleDetails.logoUrl}
+                alt="Company Logo"
+                style={{ maxWidth: "100%", marginRight: "15px" }}
+              />
+              <div>
+                <div style={{ fontWeight: "600", color: "#6C7A89" }}>
+                  Description
+                </div>
+                <div>{singleDetails.description}</div>
+              </div>
+            </div>
+          )}
+        </Col>
+        <Col md={5}>
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <FontAwesomeIcon icon={faPhone} style={{ marginRight: "10px" }} />
+            <div style={{ fontWeight: "600", color: "#6C7A89" }}>Phone</div>
+          </div>
+          <div>
+            <div>{singleDetails.phoneNo}</div>
+          </div>
+          <div style={{ display: "flex", alignItems: "center", marginTop: 10 }}>
+            <FontAwesomeIcon
+              icon={faEnvelope}
+              style={{ marginRight: "10px" }}
+            />
+            <div style={{ fontWeight: "600", color: "#6C7A89" }}>Email</div>
+          </div>
+          <div>
+            <div>{singleDetails.email}</div>
+          </div>
+        </Col>
+      </Row>
+      <Row className="mt-1">
+        
+        <Col
+          md={4}
+          style={{
+            border: "1px solid #dee2e6",
+            padding: 25,
+            borderRadius: "5px",
+         
+          }}
+        >
+          <div>
+            <h4>Company Details</h4>
+            <div
+              style={{ display: "flex", alignItems: "center", marginTop: 10 }}
+            >
+              <FontAwesomeIcon
+                icon={faInfoCircle}
+                style={{ marginRight: "10px" }}
+              />
+              <div style={{ fontWeight: "600", color: "#6C7A89" }}>
+                Description
+              </div>
+            </div>
+            <div>{singleDetails.description}</div>
+            <div
+              style={{ display: "flex", alignItems: "center", marginTop: 10 }}
+            >
+              {/* <FontAwesomeIcon
+                icon={faEnvelope}
+                style={{ marginRight: "10px" }}
+              />
+              <div style={{ fontWeight: "600", color: "#6C7A89" }}>Email</div>
+            </div>
+            <div>contact@gmail.com</div>
+            <div
+              style={{ display: "flex", alignItems: "center", marginTop: 10 }}
+            > */}
+              <FontAwesomeIcon
+                icon={faFacebook}
+                style={{ marginRight: "10px" }}
+              />
+              <div style={{ fontWeight: "600", color: "#6C7A89" }}>
+                Facebook
+              </div>
+            </div>
+            <a
+              href={singleDetails.twitterUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ color: "#BF55EC" }}
+            >
+              {singleDetails.facebookUrl}
+            </a>
+            <div
+              style={{ display: "flex", alignItems: "center", marginTop: 10 }}
+            >
+              <FontAwesomeIcon
+                icon={faInstagram}
+                style={{ marginRight: "10px" }}
+              />
+              <div style={{ fontWeight: "600", color: "#6C7A89" }}>
+                Instagram
+              </div>
+            </div>
+            <a
+              href={singleDetails.twitterUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ color: "#BF55EC" }}
+            >
+              {singleDetails.instagramUrl}
+            </a>
+            <div
+              style={{ display: "flex", alignItems: "center", marginTop: 10 }}
+            >
+              <FontAwesomeIcon
+                icon={faTwitter}
+                style={{ marginRight: "10px" }}
+              />
+              <div style={{ fontWeight: "600", color: "#6C7A89" }}>Twitter</div>
+            </div>
+            <div>
+              <a
+                href={singleDetails.twitterUrl}
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{ color: "#BF55EC" }}
+              >
+                {singleDetails.twitterUrl}
+              </a>
+            </div>
+            <div
+              style={{ display: "flex", alignItems: "center", marginTop: 10 }}
+            >
+              <FontAwesomeIcon
+                icon={faLinkedin}
+                style={{ marginRight: "10px" }}
+              />
+              <div style={{ fontWeight: "600", color: "#6C7A89" }}>
+                Linkedin
+              </div>
+            </div>
+            <div
+              style={{ display: "flex", alignItems: "center", marginTop: 5 }}
+            >
+              <a
+                href={singleDetails.linkedinUrl}
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{ color: "#BF55EC", textDecoration: "none" }}
+              >
+                {singleDetails.linkedinUrl}
+              </a>
+            </div>
+
+              {/* <div
+                style={{ display: "flex", alignItems: "center", marginTop: 10 }}
+              >
+                <FontAwesomeIcon
+                  icon={faEnvelope}
+                  style={{ marginRight: "10px" }}
+                />
+                <div style={{ fontWeight: "600", color: "#6C7A89" }}>Address</div>
+              </div>
+              <div>United State</div> */}
+          </div>
+        </Col>
+        <Col
+          md={8}
+          style={{
+            border: "1px solid #dee2e6",
+            padding: 25,
+            borderRadius: "5px",
+          }}
+        >
+          <div>
+          
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+export default Details;
---
+++ src/home.js
@@ -0,0 +1,276 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Table, Input, Button, Spin, message, Popconfirm, Modal } from "antd";
+import { saveAs } from "file-saver";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Link } from 'react-router-dom';
+const Home = () => {
+  const [url, setUrl] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [companies, setCompanies] = useState([]);
+  const [selectedRowKeys, setSelectedRowKeys] = useState([]);
+  const handleChange = (event) => {
+    setUrl(event.target.value);
+  };
+
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setLoading(true);
+    try {
+      const response = await axios.post("http://localhost:5000/scrape", {
+        url,
+      });
+      fetchCompanies();
+      message.success(response.data.message);
+    } catch (error) {
+      console.error(error);
+      message.error("Failed to scrape and save");
+    } finally {
+      setLoading(false);
+      setUrl("");
+    }
+  };
+
+  const fetchCompanies = async () => {
+    try {
+      const response = await axios.get("http://localhost:5000/companies");
+      setCompanies(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchCompanies();
+  }, []);
+
+  const onDeleteSelected = async () => {
+    if (selectedRowKeys.length === 0) {
+      message.error("Select rows to delete selected companies");
+      return;
+    }
+    setLoading(true);
+    try {
+      const response = await axios.delete("http://localhost:5000/companies", {
+        data: { ids: selectedRowKeys },
+      });
+      message.success(response.data.message);
+      fetchCompanies();
+    } catch (error) {
+      console.error(error);
+      message.error("Select row to delete selected companies");
+    } finally {
+      setLoading(false);
+      setSelectedRowKeys([]);
+    }
+  };
+
+
+  const columns = [
+    {
+      title: "Company Name",
+      dataIndex: "companyName",
+      key: "companyName",
+      width: 100,
+    },
+    {
+      title: "Company Logo",
+      dataIndex: "name",
+      key: "name",
+      render: (name, record) => (
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <img
+            src={record.logoUrl}
+            alt="Company Logo"
+            height="20"
+            style={{ marginRight: "10px" }}
+          />
+          {name}
+        </div>
+      ),
+      width: 150,
+    },
+    {
+      title: "Social Profiles",
+      key: "socialMedia",
+      render: (text, record) => (
+        <div>
+          {record.facebookUrl && (
+            <a
+              href={record.facebookUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src="../assets/facebook.png"
+                alt="Facebook"
+                style={{ marginRight: "5px", height: "20px" }}
+              />
+            </a>
+          )}
+          {record.twitterUrl && (
+            <a
+              href={record.twitterUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src="../assets/twitter.jpg"
+                alt="Twitter"
+                style={{ marginRight: "5px", height: "20px" }}
+              />
+            </a>
+          )}
+          {record.instagramUrl && (
+            <a
+              href={record.instagramUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src="../assets/insta.jpg"
+                alt="Instagram"
+                style={{ height: "20px" }}
+              />
+            </a>
+          )}
+          {record.linkedinUrl && (
+            <a
+              href={record.linkedinUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src="../assets/linkedin.jpg"
+                alt="LinkedIn"
+                style={{ height: "20px" }}
+              />
+            </a>
+          )}
+        </div>
+      ),
+      width: 150,
+    },
+    {
+      title: "Description",
+      dataIndex: "description",
+      key: "description",
+      width: 150,
+    },
+    // {
+    //   title: "Address",
+    //   dataIndex: "address",
+    //   key: "address",
+    //   width: 150,
+    // },
+    // {
+    //   title: "Phone Number",
+    //   dataIndex: "phoneNumber",
+    //   key: "phoneNumber",
+    //   width: 150,
+    // },
+    // {
+    //   title: "Email",
+    //   dataIndex: "email",
+    //   key: "email",
+    //   width: 150,
+    // },
+    {
+        title: "Actions",
+        key: "actions",
+        render: (text, record) => (
+            <Link to={`/details/${record._id}`}>
+            <Button>View</Button>
+          </Link>
+        ),
+        width: 100,
+        align: "center",
+      },
+  ];
+
+  const rowSelection = {
+    selectedRowKeys,
+    onChange: (selectedRowKeys, selectedRows) => {
+      setSelectedRowKeys(selectedRowKeys);
+    },
+  };
+
+  const downloadCSV = () => {
+    const header = Object.keys(companies[0]);
+    const csvData = [
+      header.join(","),
+      ...companies.map((company) =>
+        header.map((fieldName) => company[fieldName]).join(",")
+      ),
+    ].join("\n");
+
+    const blob = new Blob([csvData], { type: "text/csv;charset=utf-8" });
+    saveAs(blob, "companies.csv");
+  };
+
+  return (
+    <div className="container">
+      <form onSubmit={handleSubmit} className="mt-4">
+        <label>
+          Enter domain name:
+          <Input
+            type="text"
+            value={url}
+            onChange={handleChange}
+            placeholder="Enter domain name"
+          />
+        </label>
+        <Button
+          type="default"
+          htmlType="submit"
+          style={{ marginLeft: 10, background: "rgb(161 248 255)" }}
+        >
+          Fetch & save details
+        </Button>
+      </form>
+
+      {loading ? (
+        <div className="text-center mt-4">
+          <Spin size="large" />
+        </div>
+      ) : (
+        <>
+          <div className="mt-4">
+            <button
+              type="submit"
+              className="btn btn-default btn-sm"
+              style={{ background: "rgb(161 248 255)", marginRight: 10 }}
+              onClick={onDeleteSelected}
+            >
+              Delete Selected
+            </button>
+            <button
+              type="submit"
+              className="btn btn-default btn-sm"
+              onClick={downloadCSV}
+              style={{ marginLeft: 10, background: "rgb(161 248 255)" }}
+            >
+              Export as CSV
+            </button>
+          </div>
+          <div className="mt-4">
+            <Table
+              rowSelection={{
+                type: "checkbox",
+                ...rowSelection,
+              }}
+              columns={columns}
+              dataSource={companies}
+              rowKey="_id"
+              scroll={{ x: "max-content" }}
+            />
+          </div>
+        
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Home;
